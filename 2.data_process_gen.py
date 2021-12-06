50794d905a00127f1b2263e3f3adc031ccf86d21
vic.hong <vic.hong@rock-chips.com>
2021-12-06 17:35:15 +0800

    整合数据处理

---
+++ 2.data_process_gen.py
@@ -0,0 +1,115 @@
+#!/opt/homebrew/anaconda3/bin/python
+# 程序说明：
+# 从一个或者多个文件中读取一列数字，检查这一列数字是否为递增、将数据去重排序、将数据绘制到同一个坐标系中
+import sys
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+def loadData(fname):
+    f = open(fname)
+    lines = f.readlines()
+    
+    nums = []
+    for line in lines:
+        cur = int(line.strip('\n'))
+        nums.append(cur)
+
+    return nums
+
+def loadDataGrp(fnames):
+    dataGrp = []
+
+    for i in range(len(fnames)):
+        nums = loadData(fnames[i])
+        dataGrp.append(nums)
+
+    return dataGrp 
+
+def dumpListToFile(fname, ldata):
+    f = open(fname, "w")
+    for i in ldata:
+        f.write(str(i)+"\n")
+    f.close()
+
+def compList(list1, list2):
+    print("list1 size %d list2 size %d" % (len(list1), len(list2)))
+    loopMax = len
+    if len(list1) < len(list2):
+        loopMax = len(list1)
+    else:
+        loopMax = len(list2)
+
+    print("list1 != list2 in index ", end='')
+    for i in range(loopMax):
+        if list1[i] != list2[i]:
+            print(" ", i, end='')
+    print()
+
+def delRepAndSort(nums):
+    newNums = list(set(nums))
+    newNums.sort()
+    # print(nums)
+    # print(newNums)
+    return newNums
+
+def checkNumsInc(nums):
+    lineNum = 0
+    prev = 0
+    cur = 0
+    print("\tnot increase in lineNum: ", end = '')
+    for i in range(len(nums)):
+        lineNum = lineNum + 1
+        cur = nums[i]
+        if prev > cur:
+            print(" ", lineNum, end='')
+        prev = cur
+    print()
+    return
+
+global_color  = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
+global_marker = ['.', ',', 'o', 'v', '^', '<', '1', '2', '3', '4', '8']
+def plotPts(fileNames, dataGrp):
+    if len(fileNames) != len(dataGrp):
+        print("error: file cnt and data cnt is not equal")
+        print("file cnt is %d data cnt is %d" % (len(fileNames), (len(dataGrp))))
+        return
+
+    fig = plt.figure()  # an empty figure with no Axes
+    ax = fig.add_subplot()
+    loopCnt = len(fileNames)
+
+    for i in range(loopCnt):
+        x = list(range(len(dataGrp[i])))
+        ax.plot(x, dataGrp[i], marker=global_marker[i], color=global_color[i], linestyle='', label=fileNames[i])  # Plot some data on the axes.
+        print("%s data size:%d" % (fileNames[i], len(dataGrp[i])))
+
+    ax.legend()  # Add a legend.
+    plt.show()
+
+
+def main():
+    # dataGrpCnt = int(input("Please enter the cnt of data group: "))
+    # fileNames = []
+    # for i in range(dataGrpCnt):
+    #     filename = str(input("Please enter file %d name: " % i))
+    #     fileNames.append(filename)
+
+    dataGrpCnt = len(sys.argv) - 1
+    fileNames = []
+    for i in range(dataGrpCnt):
+        fileNames.append(str(sys.argv[i+1]))
+
+    dataGrp =  loadDataGrp(fileNames)
+    print("==================== result ====================")
+    for i in range(dataGrpCnt):
+        print("====> %s" % fileNames[i])
+        checkNumsInc(dataGrp[i])
+        dataGrp[i] = delRepAndSort(dataGrp[i])
+        
+    print()
+    plotPts(fileNames, dataGrp)
+
+
+if __name__ == '__main__':
+    main()
