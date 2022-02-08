79d8b4540e497690b0b6e962af9a66b375e0e207
Hongjin Li <vic.hong@rock-chips.com>
2022-02-08 10:34:57 +0800

    添加adbkill脚本

---
+++ 7.adbkill.sh
@@ -0,0 +1,12 @@
+#########################################################################
+# File Name: adbkill.sh
+# Author: LiHongjin
+# mail: 872648180@qq.com
+# Created Time: 2021年12月28日 星期二 20时42分39秒
+#########################################################################
+#!/bin/bash
+
+adb shell pkill mediaserver
+adb shell pkill cameraserver
+adb shell killall media.codec
+adb shell killall rockchip.hardware.rockit.hw@1.0-service
