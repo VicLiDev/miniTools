bbe5dfe68a4545c102f050c12800d1e7926396d5
Hongjin Li <vic.hong@rock-chips.com>
2022-12-06 20:30:10 +0800

    reg opt add cmd para

--- 13.reg_opt/main.py
+++ 13.reg_opt/main.py
@@ -10,30 +10,42 @@ import scatterDisplay
 def main():
     # parser params
     parser = argparse.ArgumentParser(description='reg analyze')
-    parser.add_argument('-f', type=str, help="reg excele")
+    parser.add_argument('-i', type=str, default="", help="reg excele")
+    parser.add_argument('-o', type=str, default="", help="reg excele")
+    parser.add_argument('-c', action='store_true', default=False, help="check reg")
+    parser.add_argument('-g', type=str, default="", help="generate chead")
+    parser.add_argument('-p', action='store_true', default=False, help="plot reg scatter")
     args = parser.parse_args()
-    fileName = args.f
-    if fileName == "":
-        print(args.help)
+    print(args)
 
-    # load write excel
-    regSet = excel_rw.load_excel(fileName)
+    # load excel
+    if args.i == "":
+        exit(0)
+    regSet = excel_rw.load_excel(args.i)
+
+    # sort
     regSet.sort_regs()
     regSet.sort_fields()
-    # print(regSet)
-    # save_regs_to_file("output.xlsx", regSet)
+
+    # write excel
+    if args.o:
+        save_regs_to_file(args.o, regSet)
 
     # check reg
-    regproc.check_bit_reuse(regSet)
+    if args.c:
+        regproc.check_bit_reuse(regSet)
 
+    # print(regSet)
     # gen c head
-    gen_chead.add_file_head("vdpu_383.h")
-    gen_chead.gen_CHead_seg("vdpu_383.h", regSet, "Vdpu383RegVersion", 0, 0)
-    gen_chead.gen_CHead_seg("vdpu_383.h", regSet, "Vdpu383RegTest", 1, 20)
-    gen_chead.add_file_tail("vdpu_383.h")
+    if args.g:
+        gen_chead.add_file_head(args.g)
+        gen_chead.gen_CHead_seg(args.g, regSet, "Vdpu383RegVersion", 0, 0)
+        gen_chead.gen_CHead_seg(args.g, regSet, "Vdpu383RegTest", 1, 20)
+        gen_chead.add_file_tail(args.g)
 
     # plot
-    scatterDisplay.plotRegs(regSet)
+    if args.p:
+        scatterDisplay.plotRegs(regSet)
 
     
 
