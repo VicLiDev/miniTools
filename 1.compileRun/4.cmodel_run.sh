3e223960c264e2169deb058fb7be79212fb44ed8
Hongjin Li <vic.hong@rock-chips.com>
2023-06-15 16:03:23 +0800

    update cmodel build/run/debug/test script

--- 1.compileRun/4.cmodel_run.sh
+++ 1.compileRun/4.cmodel_run.sh
@@ -1,7 +1,49 @@
 #!/bin/bash
+
+# example: lhjRun.sh <proc> [<b>] [<bt>] [<gdb>] [<batch>]
 clear
 
 rootDir="/local/lhj/Projects/c_model2"
+cmd_proc="null"
+cmd_build=false
+cmd_build_test=false
+cmd_debug=false
+cmd_batch=false
+
+for para in $*
+do
+    case ${para} in
+    "av1" | "hevc" | "avs2" | "vp9" | "avc")
+        cmd_proc=${para}
+        ;;
+    "b")
+        cmd_build=true
+        ;;
+    "bt")
+        cmd_build_test=true
+        ;;
+    "gdb")
+        cmd_debug=true
+        ;;
+    "batch")
+        cmd_batch=true
+        ;;
+    *)
+        echo "unsupport cmd"
+        ;;
+    esac
+done
+
+if [ cmd_proc == "null" ]; then
+    echo "proc is null"
+    exit 0
+fi
+
+echo "proc: $cmd_proc"
+echo "build: $cmd_build"
+echo "build test: $cmd_build_test"
+echo "debug: $cmd_debug"
+echo "batch test: $cmd_batch"
 
 # ============ check ============
 function resultCheck()
@@ -11,10 +53,16 @@ function resultCheck()
     do
         # file1="testOut/Frame000${loop}/loopfilter_sao_out_check.txt"
         # file2="testOut/Frame000${loop}/loopfilter_sao_data.dat"
-        file1="testOut/Frame000${loop}/loopfilter_data.dat"
-        file2="testOut/Frame000${loop}/loopfilter_data_ver.dat"
+
+        # file1="testOut/Frame000${loop}/loopfilter_data.dat"
+        # file2="testOut/Frame000${loop}/loopfilter_data_ver.dat"
+
         # file1="testOut/Frame000${loop}/loopfilter_origin_data.dat"
         # file2="testOut/Frame000${loop}/loopfilter_origin_data_ver.dat"
+
+        file1="testOut/Frame000${loop}/filterd_dblk_mid_ver.dat"
+        file2="Frame000${loop}/filterd_dblk_mid_ver.dat"
+
         if [[ -e ${file1} ]] && [[ -e ${file2} ]]; then
             # echo "file1: ${file1}"
             # echo "file2: ${file2}"
@@ -39,9 +87,16 @@ function buildAv1()
     # make clean &&
     #     cmake ..  &&
     #     make -j 10
-    make clean &&
-        cmake -DCMAKE_BUILD_TYPE=Debug ..  &&
-        make -j 10
+    make clean
+    cmake .. \
+        -DCMAKE_BUILD_TYPE=Debug \
+        -DCONFIG_AV1_ENCODER=0 \
+        -DCONFIG_AV1_DECODER=1 \
+        -DAOM_TARGET_CPU=generic \
+        -DCONFIG_MULTITHREAD=0 \
+        -DENABLE_DOCS=0 \
+        -DCONFIG_HARDWARE=1 &&
+    make -B -j 10
 }
 
 function buildHevc()
@@ -97,8 +152,8 @@ function buildVp9()
         --disable-avx512 \
         --as=yasm \
         --enable-hardware &&
-        make clean &&
-        make -B
+        make -B -j 6
+        # make clean &&
 }
 
 function buildAvc()
@@ -109,25 +164,60 @@ function buildAvc()
 
 # ============ run ============
 
-function runAv1Sig()
+function runSig()
 {
     av1Cmd="./aomdec \
         -i /path/to/video \
         -o testOut/output.yuv \
         -b 0 \
-        -e 2 \
+        -e 10 \
         -c av1_vdp_cfg \
         -g av1_cmodel_cfg \
-        -f loopfilter=0xFFFFFFFFFFFFFFFF \
+        -f loopfilter=0xF \
         -d testOut"
 
-    if [ "$1" == "gdb" ];then
-        gdb --command=debug.gdb --args $av1Cmd
+    hevcCmd="./hm \
+        -i /path/to/video \
+        -b 0 \
+        -e 10 \
+        -g hevc_cmodel_cfg \
+        -f loopfilter=0xF \
+        -d testOut"
+
+    vp9Cmd="./vpxdec \
+        -i /path/to/video \
+        -o testOut/rec.yuv \
+        -b 0 \
+        -e 10 \
+        -g vp9_cmodel_cfg \
+        -f loopfilter=0xF \
+        -d testOut"
+
+    avs2Cmd="./Lbuild/bin/ldecod \
+        -i /path/to/video \
+        -o output/rec.yuv \
+        -b 7 \
+        -e 7 \
+        -f loopfilter=0xF \
+        -g source/bin/avs2_file_cmodel_cfg \
+        -d testOut"
+
+    avcCmd="./build/jm \
+        -i /path/to/video \
+        -b 0 \
+        -e 0 \
+        -g build/h264_cmodel_cfg \
+        -f loopfilter=0xF \
+        -d build"
+
+    eval runCmd='$'${1}Cmd
+    if [ "$2" == true ];then
+        gdb --command=debug.gdb --args $runCmd
     else
-        $av1Cmd
+        $runCmd
     fi
 
-    echo "cmd: ${av1Cmd}"
+    echo "cmd: $runCmd"
 }
 
 function runAv1Batch()
@@ -137,27 +227,9 @@ function runAv1Batch()
         && bash ~/Projects/batch_test.sh av1 /test_data/movie_video_testdata/av1
 }
 
-function runHevcSig()
-{
-    hevcCmd="./hm \
-        -i /path/to/video \
-        -b 0 \
-        -e 10 \
-        -g hevc_cmodel_cfg \
-        -f loopfilter=0xFFFFFFFFFFFFFFFF,decctl=0x7 \
-        -d testOut"
-
-    if [ "$1" == "gdb" ];then
-        gdb --command=debug.gdb --args $hevcCmd
-    else
-        $hevcCmd
-    fi
-    echo "cmd: ${hevcCmd}"
-}
-
 function runHevcBatch()
 {
-    bash ~/Projects/batch_test.sh hevc /test_data/allegro_hevc_stream 732 \
+    bash ~/Projects/batch_test.sh hevc /test_data/allegro_hevc_stream \
         && bash ~/Projects/batch_test.sh hevc /test_data/argon_streams_hevc_rockchip \
         && bash ~/Projects/batch_test.sh hevc /test_data/argon_streams_hevc_rockchip2 \
         && bash ~/Projects/batch_test.sh hevc /test_data/argon_streams_hevc_rockchip4 \
@@ -173,25 +245,6 @@ function runHevcBatch()
         && bash ~/Projects/batch_test.sh hevc /test_data/super_resolution_stream/encoder_test/hevc
 }
 
-function runAvs2Sig()
-{
-    avs2Cmd="./Lbuild/bin/ldecod \
-        -i /path/to/video \
-        -o output/rec.yuv \
-        -b 1 \
-        -e 3 \
-        -c source/bin/avs2_vdp_dec_cfg \
-        -g source/bin/avs2_file_cmodel_cfg \
-        -d testOut"
-
-    if [ "$1" == "gdb" ];then
-        gdb --command=debug.gdb --args $avs2Cmd
-    else
-        $avs2Cmd
-    fi
-    echo "cmd: ${avs2Cmd}"
-}
-
 function runAvs2Batch()
 {
     bash ~/Projects/batch_test.sh avs2 /test_data/AVS2_allegro \
@@ -203,49 +256,12 @@ function runAvs2Batch()
         && bash ~/Projects/batch_test.sh avs2 /test_data/super_resolution_stream/avs2
 }
 
-function runVp9Sig()
-{
-    vp9Cmd="./vpxdec \
-        -i /path/to/video \
-        -o testOut/rec.yuv \
-        -b 0 \
-        -e 2 \
-        -c vp9_vdp_cfg \
-        -g vp9_cmodel_cfg \
-        -f loopfilter=0xFFFFFFFFFFFFFFFF \
-        -d testOut"
-
-    if [ "$1" == "gdb" ];then
-        gdb --command=debug.gdb --args $vp9Cmd
-    else
-        $vp9Cmd
-    fi
-    echo "cmd: ${vp9Cmd}"
-}
-
 function runVp9Batch()
 {
     bash ~/Projects/batch_test.sh vp9 /test_data/argon_streams_vp9_rockchip \
         && bash ~/Projects/batch_test.sh vp9 /test_data/fpga_packet/normal_resolution/vp9 \
         && bash ~/Projects/batch_test.sh vp9 /test_data/fpga_packet/super_resolution/vp9 \
         && bash ~/Projects/batch_test.sh vp9 /test_data/super_resolution_stream/vp9
-}
-
-function runAvcSig()
-{
-    avcCmd="./build/jm \
-        -i /path/to/video \
-        -b 0 \
-        -e 3 \
-        -g build/h264_cmodel_cfg \
-        -d build"
-
-    if [ "$1" == "gdb" ];then
-        gdb --command=debug.gdb --args $avcCmd
-    else
-        $avcCmd
-    fi
-    echo "cmd: ${avcCmd}"
 }
 
 function runAvcBatch()
@@ -260,85 +276,32 @@ function runAvcBatch()
         # && bash ~/Projects/batch_test.sh avc /test_data/super_resolution_stream/encoder_test/h264
 }
 
-case $1 in
-    "av1")
-        if [ "$2" = "b" ]; then
-            buildAv1
+# ============ main ============
+if [ $cmd_build_test == true ]; then
+    cd "${rootDir}/c_model_av1/build/" && buildAv1 && runSig av1
+    read -p "continue? [y/n]:" runOpt; if [ "$runOpt" == "n" ];then exit 0; fi
+    
+    cd "${rootDir}/c_model_avs2/RD19.5/" && buildAvs2 && runSig avs2
+    read -p "continue? [y/n]:" runOpt; if [ "$runOpt" == "n" ];then exit 0; fi
+    
+    cd "${rootDir}/c_model_h264_v2/jm18.6/" && buildAvc && runSig avc
+    read -p "continue? [y/n]:" runOpt; if [ "$runOpt" == "n" ];then exit 0; fi
+    
+    cd "${rootDir}/c_model_hevc_v2/build/" && buildHevc && runSig hevc
+    read -p "continue? [y/n]:" runOpt; if [ "$runOpt" == "n" ];then exit 0; fi
+    
+    cd "${rootDir}/c_model_vp9_10bit/libvpx-1.11.0/build/" && buildVp9 && runSig vp9
+else
+    if [ $cmd_build == true ]; then
+        build${cmd_proc^}
+    fi
+    if [ $? -eq 0 ]; then
+        if [ $cmd_batch == true ]; then
+            run${cmd_proc^}Batch
+        else
+            runSig $cmd_proc $cmd_debug
         fi
-        if [ $? -eq 0 ]; then
-            if [ "$3" = "batch" ]; then
-                runAv1Batch
-            else
-                runAv1Sig $3
-            fi
-        fi
-        ;;
-    "hevc")
-        if [ "$2" = "b" ]; then
-            buildHevc
-        fi
-        if [ $? -eq 0 ]; then
-            if [ "$3" = "batch" ]; then
-                runHevcBatch
-            else
-                runHevcSig $3
-            fi
-        fi
-        ;;
-    "avs2")
-        if [ "$2" = "b" ]; then
-            buildAvs2
-        fi
-        if [ $? -eq 0 ]; then
-            if [ "$3" = "batch" ]; then
-                runAvs2Batch
-            else
-                runAvs2Sig $3
-            fi
-        fi
-        ;;
-    "vp9")
-        if [ "$2" = "b" ]; then
-            buildVp9
-        fi
-        if [ $? -eq 0 ]; then
-            if [ "$3" = "batch" ]; then
-                runVp9Batch
-            else
-                runVp9Sig $3
-            fi
-        fi
-        ;;
-    "avc")
-        if [ "$2" = "b" ]; then
-            buildAvc
-        fi
-        if [ $? -eq 0 ]; then
-            if [ "$3" = "batch" ]; then
-                runAvcBatch
-            else
-                runAvcSig $3
-            fi
-        fi
-        ;;
-    "bt")
-        cd "${rootDir}/c_model_av1/build/" && buildAv1 && runAv1Sig
-        read -p "continue? [y/n]:" runOpt; if [ "$runOpt" == "n" ];then exit 0; fi
-
-        cd "${rootDir}/c_model_avs2/RD19.5/" && buildAvs2 && runAvs2Sig
-        read -p "continue? [y/n]:" runOpt; if [ "$runOpt" == "n" ];then exit 0; fi
-
-        cd "${rootDir}/c_model_h264_v2/jm18.6/" && buildAvc && runAvcSig
-        read -p "continue? [y/n]:" runOpt; if [ "$runOpt" == "n" ];then exit 0; fi
-
-        cd "${rootDir}/c_model_hevc_v2/build/" && buildHevc && runHevcSig
-        read -p "continue? [y/n]:" runOpt; if [ "$runOpt" == "n" ];then exit 0; fi
-
-        cd "${rootDir}/c_model_vp9_10bit/libvpx-1.11.0/build/" && buildVp9 && runVp9Sig
-        ;;
-    *)
-        echo "unsupport proc"
-        ;;
-esac
+    fi
+fi
 
 # resultCheck
