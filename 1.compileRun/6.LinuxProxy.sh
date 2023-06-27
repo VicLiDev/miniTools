f6e6b7a4c2720078cd7a7793cf8cdc808f7e26ad
Hongjin Li <vic.hong@rock-chips.com>
2023-06-27 21:28:09 +0800

    add linux proxy config

---
+++ 1.compileRun/6.LinuxProxy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#########################################################################
+# File Name: proxy.sh
+# Author: LiHongjin
+# mail: 872648180@qq.com
+# Created Time: 2023年06月27日 星期二 19时57分23秒
+#########################################################################
+
+# 配置文件可以从其他系统的clash里copy
+nohup clash-linux-386-v1.16.0 -f ~/.config/clash/agentNeo.yaml &
+
+# set in .zshrc
+# export http_proxy=http://127.0.0.1:8090
+# export https_proxy=http://127.0.0.1:8090
+# export all_proxy=socks5://127.0.0.1:8091
+
+# manual set
+# git config --global http.proxy http://127.0.0.1:8890
+# git config --global https.proxy https://127.0.0.1:8890
+# git config --global https.proxy socks5://127.0.0.1:8091
