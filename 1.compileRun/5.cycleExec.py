a0f0db5f671e8c7f4c3053278791f784a21fd0ec
Hongjin Li <vic.hong@rock-chips.com>
2023-09-13 09:36:07 +0800

    update cycleExec

--- 1.compileRun/5.cycleExec.py
+++ 1.compileRun/5.cycleExec.py
@@ -8,26 +8,37 @@
 
 # 参考： https://zhuanlan.zhihu.com/p/501021369
 
-import schedule
-import time
-import datetime
+try:
+    import schedule
+    import time
+    import datetime
+except Exception as err_info:
+    print("errinfo:")
+    print(err_info)
+    exit(0)
+
 
 # 定义你要周期运行的函数
 def job():
     print("I'm working...")
+
+def job2():
+    print("I'm working2...")
 
 if __name__ == '__main__':
     # 调用scedule.every(间隔).时间类型.do(job) 发布周期任务
     # 发布后的周期任务需要用run_pending函数来检测是否执行，因此需要一个While循环不断地轮询这个函数。
 
     # 按时间间隔执行
-    # schedule.every().second.do(job)
+    schedule.every().second.do(job)
+    schedule.every().second.do(job2)
+    # schedule.every(1).minutes.do(job)                # 每隔 1 分钟运行一次 job 函数
     # schedule.every(10).minutes.do(job)               # 每隔 10 分钟运行一次 job 函数
     # schedule.every().hour.do(job)                    # 每隔 1 小时运行一次 job 函数
 
     # 按时间周期执行
     # schedule.every().minute.at(":17").do(job)        # 每分钟的 17 秒时间点运行 job 函数
-    schedule.every().day.at("10:30").do(job)         # 每天在 10:30 时间点运行 job 函数
+    # schedule.every().day.at("10:30").do(job)         # 每天在 10:30 时间点运行 job 函数
     # schedule.every().monday.do(job)                  # 每周一 运行一次 job 函数
     # schedule.every().wednesday.at("13:15").do(job)   # 每周三 13：15 时间点运行 job 函数
 
@@ -47,13 +58,14 @@ if __name__ == '__main__':
     # friends = schedule.get_jobs('friend')
     # # 取消所有 daily-tasks 标签的任务
     # schedule.clear('daily-tasks')
-    
+
     # 如果某个机制触发了，你需要立即运行所有作业，可以调用schedule.run_all():
     # schedule.run_all()
     # 立即运行所有作业，每次作业间隔10秒
     # schedule.run_all(delay_seconds=10)
     all_jobs = schedule.get_jobs()
-    print(all_jobs)
+    for job_tmp in all_jobs:
+        print(job_tmp)
 
     # schedule其实就只是个定时器。在while True死循环中，schedule.run_pending()是保持schedule一直运行，去查询上面那一堆的任务
     while True:
