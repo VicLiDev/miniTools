c9334920b17709c20b85caca4473423dba082610
Hongjin Li <vic.hong@rock-chips.com>
2023-11-23 11:30:44 +0800

    update cmp dir tool

--- 2.dataProc/12.cmp_dir.sh
+++ 2.dataProc/12.cmp_dir.sh
@@ -7,7 +7,7 @@
 #########################################################################
 
 # compare all files in two dir
-# ex: bash ./12.check_data.sh <dir1/file1> <dir2/file2>
+# ex: bash ./12.check_data.sh <dir1/file1> <dir2/file2> [q]
 
 if [ $# -lt 2 ]; then
     echo "too few parameters"
@@ -16,21 +16,34 @@ if [ $# -lt 2 ]; then
 fi
 dataDir1=$1
 dataDir2=$2
+diffQuit=$3
+cmpRet=0
+
+if [ ${dataDir1:0:1} != "/" ]; then echo "Please input abs path"; exit 1; fi
+if [ ${dataDir2:0:1} != "/" ]; then echo "Please input abs path"; exit 1; fi
 
 if [ `find ${dataDir1} | wc -l` -ne `find ${dataDir2} | wc -l` ]; then
     echo "dir1 file cnt: `find ${dataDir1} | wc -l`"
     echo "dir2 file cnt: `find ${dataDir2} | wc -l`"
-    exit 1
+    if [ "$diffQuit" == "q" ];then exit 1; fi
 fi
 
 for file in `find ${dataDir1} | sort`
 do
-    file1=${dataDir1}${file##*${dataDir1}}
-    file2=${dataDir2}${file##*${dataDir1}}
+    file1="${dataDir1}${file##*${dataDir1}}"
+    file2="${dataDir2}${file##*${dataDir1}}"
 
-    if [ ! -e ${file1} ]; then echo "${file1} not exist"; exit 1; fi
-    if [ ! -e ${file2} ]; then echo "${file2} not exist"; exit 1; fi
     if [ ! -f ${file1} ] && [ ! -f ${file2} ]; then continue; fi
+    if [ ! -e ${file1} ]; then
+        cmpRet=1
+        echo "--> [res]: ${file1} not exist"
+        if [ "$diffQuit" == "q" ];then exit 1; else continue; fi
+    fi
+    if [ ! -e ${file2} ]; then
+        cmpRet=1
+        echo "--> [res]: ${file2} not exist"
+        if [ "$diffQuit" == "q" ];then exit 1; else continue; fi
+    fi
 
     frmIdx="${file1##*Frame}"
     frmIdx="${frmIdx##*frame}"
@@ -43,10 +56,12 @@ do
     md5Val1=`md5sum ${file1} | awk '{print $1}'`
     md5Val2=`md5sum ${file2} | awk '{print $1}'`
     if [ $md5Val1 != $md5Val2 ]; then
+        cmpRet=1
         echo "--> [res]: ${frmIdx} file: ${fileNmae} compare faile    vimdiff ${file1} ${file2}"
-        exit 1
+        if [ "$diffQuit" == "q" ];then exit 1; fi
     else
         echo "--> [res]: ${frmIdx} file: ${fileNmae} compare pass"
     fi
-    
 done
+
+exit $cmpRet
