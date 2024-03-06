124c97a62cfddaa06a8f60f0160d25e7356f09b7
Hongjin Li <vic.hong@rock-chips.com>
2024-03-06 19:09:05 +0800

    update ffmpeg build script framework

--- 1.compileRun/1.ffmpegbuild.sh
+++ 1.compileRun/1.ffmpegbuild.sh
@@ -8,181 +8,170 @@
 
 set -e
 
+create_dir()
+{
+    cur_dir=$1
+    if [ ! -d "${cur_dir}" ]; then
+        echo "create dir ${cur_dir}"
+        mkdir -p ${cur_dir}
+    fi
+}
+
 setup_env()
 {
-    # create directory
-    if [ ! -d "${HOME}/Projects/ffmpeg_source" ]; then
-        echo "create dir ${HOME}/Projects/ffmpeg_source"
-        mkdir ~/Projects/ffmpeg_source
-    fi
-    if [ ! -d "${HOME}/Projects/ffmpeg_source/bin" ]; then
-        echo "create dir ${HOME}/Projects/ffmpeg_source/bin"
-        mkdir ~/Projects/ffmpeg_source/bin
-    fi
-    if [ ! -d "${HOME}/Projects/ffmpeg_source/ffmpeg_build" ]; then
-        echo "create dir ${HOME}/Projects/ffmpeg_source/ffmpeg_build"
-        mkdir ~/Projects/ffmpeg_source/ffmpeg_build
-    fi
-    
     # set env
-    FFMPEGPATH="$HOME/Projects/ffmpeg_source/bin:$PATH" && \
-        FFMPEGPREFIX="$HOME/Projects/ffmpeg_source/ffmpeg_build" && \
-        FFMPEGBIN="$HOME/Projects/ffmpeg_source/bin" && \
-        FFMPEGPKGCONFIG="$HOME/Projects/ffmpeg_build/lib/pkgconfig" && \
-        FFMPEGROOT="$HOME/Projects/ffmpeg_source"
+    FFMPEGROOT="$HOME/Projects/ffmpeg_source"
+    FFMPEGPREFIX="${FFMPEGROOT}/ffmpeg_build"
+    FFMPEGBIN="${FFMPEGPREFIX}/bin"
+    FFMPEGPKGCONFIG="${FFMPEGPREFIX}/lib/pkgconfig"
+    FFMPEGPATH="${FFMPEGBIN}:$PATH"
+
+    # create directory
+    create_dir ${FFMPEGROOT}
+    create_dir ${FFMPEGBIN}
+    create_dir ${FFMPEGPREFIX}
 }
 
 compile_nasm()
 {
     #-- nasm
-    cd $FFMPEGROOT
-    echo "======> compile nasm <======"
-    if [ ! -d "nasm-2.15.05" ]; then
-        mkdir nasm-2.15.05
-    fi
-    cd nasm-2.15.05
-    ./autogen.sh && PATH=$FFMPEGPATH ./configure --prefix=$HOME/ffmpeg_build --bindir=$FFMPEGBIN
+    wk_dir="nasm-2.15.05"
+    create_dir ${wk_dir} && cd ${wk_dir}
+
+    ./autogen.sh && PATH=$FFMPEGPATH ./configure \
+        --prefix=$HOME/ffmpeg_build --bindir=$FFMPEGBIN
     make -j20 && make install
-    echo "======> compile nasm finish <======"
 }
 
 compile_x264()
 {
     #-- 264
-    cd $FFMPEGROOT
-    echo "======> compile x264 <======"
-    if [ ! -d "x264" ]; then
-        mkdir x264
-    fi
-    cd x264
-    PATH=$FFMPEGPATH PKG_CONFIG_PATH=$FFMPEGPKGCONFIG ./configure --prefix=$FFMPEGPREFIX --bindir=$FFMPEGBIN --enable-static --enable-pic
+    wk_dir="x264"
+    create_dir ${wk_dir} && cd ${wk_dir}
+
+    # PATH=$FFMPEGPATH PKG_CONFIG_PATH=$FFMPEGPKGCONFIG ./configure \
+    PATH=$FFMPEGPATH ./configure \
+        --prefix=$FFMPEGPREFIX --bindir=$FFMPEGBIN \
+		--enable-static --enable-pic
     PATH=$FFMPEGPATH make -j 20 && make install
-    echo "======> compile x264 finish <======"
 }
 
 compile_x265()
 {
     #-- 265
-    cd $FFMPEGROOT
-    echo "======> compile x265 <======"
-    if [ ! -d "multicoreware-x265_git-e3713124dccd/build/linux" ]; then
-        mkdir multicoreware-x265_git-e3713124dccd/build/linux
-    fi
-    cd multicoreware-x265_git-e3713124dccd/build/linux
-    PATH=$FFMPEGPATH cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$FFMPEGPREFIX -DENABLE_SHARED=off ../../source
+    wk_dir="multicoreware-x265_git-e3713124dccd/build/linux"
+    create_dir ${wk_dir} && cd ${wk_dir}
+
+    PATH=$FFMPEGPATH cmake -G "Unix Makefiles" \
+        -DCMAKE_INSTALL_PREFIX=$FFMPEGPREFIX -DENABLE_SHARED=off ../../source
     PATH=$FFMPEGPATH make -j20 && make install
-    echo "======> compile x265 finish <======"
 }
 
 compile_vpx()
 {
     #-- vpx
-    cd $FFMPEGROOT
-    echo "======> compile vpx <======"
-    if [ ! -d "libvpx-1.9.0" ]; then
-        mkdir libvpx-1.9.0
-    fi
-    cd libvpx-1.9.0
-    PATH=$FFMPEGPATH ./configure --prefix=$FFMPEGPREFIX --disable-examples --disable-unit-tests --enable-vp9-highbitdepth --as=yasm
+    wk_dir="libvpx-1.9.0"
+    create_dir ${wk_dir} && cd ${wk_dir}
+
+    PATH=$FFMPEGPATH ./configure \
+        --prefix=$FFMPEGPREFIX \
+        --disable-examples \
+        --disable-unit-tests \
+        --enable-vp9-highbitdepth \
+        --as=yasm
     PATH=$FFMPEGPATH make -j 20 && make install
-    echo "======> compile vpx finish <======"
 }
 
 compile_fdkaac()
 {
     #-- fdkaac
-    cd $FFMPEGROOT
-    echo "======> compile fdkaac <======"
-    if [ ! -d "fdk-aac-master" ]; then
-        mkdir fdk-aac-master
-    fi
-    cd fdk-aac-master
+    wk_dir="fdk-aac-master"
+    create_dir ${wk_dir} && cd ${wk_dir}
+
     autoreconf -fiv && ./configure --prefix=$FFMPEGPREFIX --disable-shared
     make -j20 && make install
-    echo "======> compile fdkaac finish <======"
 }
 
 compile_opus()
 {
     #-- opus
-    cd $FFMPEGROOT
-    echo "======> compile opus <======"
-    if [ ! -d "opus-master" ]; then
-        mkdir opus-master
-    fi
-    cd opus-master
+    wk_dir="opus-master"
+    create_dir ${wk_dir} && cd ${wk_dir}
+
     ./autogen.sh && ./configure --prefix=$FFMPEGPREFIX --disable-shared
     make -j 20 && make install
-    echo "======> compile opus finish <======"
 }
 
 compile_aom()
 {
     #-- aom
-    cd $FFMPEGROOT
-    echo "======> compile aom <======"
-    if [ ! -d "aom_build" ]; then
-        mkdir aom_build
-    fi
-    cd aom_build
-    PATH="$FFMPEGPATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$FFMPEGPREFIX -DENABLE_TESTS=OFF -DENABLE_NASM=on ../aom
+    wk_dir="aom_build"
+    create_dir ${wk_dir} && cd ${wk_dir}
+
+    PATH="$FFMPEGPATH" cmake \
+        -G "Unix Makefiles" \
+        -DCMAKE_INSTALL_PREFIX=$FFMPEGPREFIX \
+        -DENABLE_TESTS=OFF \
+        -DENABLE_NASM=on \
+        ../aom
     PATH=$FFMPEGPATH make -j20 && make install
-    echo "======> compile aom finish <======"
 }
 
 compile_svt()
 {
     #-- svt
-    cd $FFMPEGROOT
-    echo "======> compile svt <======"
-    if [ ! -d "SVT-AV1/build" ]; then
-        mkdir SVT-AV1/build
-    fi
-    cd SVT-AV1/build
-    PATH=$FFMPEGPATH cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$FFMPEGPREFIX -DCMAKE_BUILD_TYPE=Release -DBUILD_DEC=OFF -DBUILD_SHARED_LIBS=OFF ..
+    wk_dir="SVT-AV1/build"
+    create_dir ${wk_dir} && cd ${wk_dir}
+
+    PATH=$FFMPEGPATH cmake \
+        -G "Unix Makefiles" \
+        -DCMAKE_INSTALL_PREFIX=$FFMPEGPREFIX \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DBUILD_DEC=OFF \
+        -DBUILD_SHARED_LIBS=OFF ..
     PATH=$FFMPEGPATH make -j20 && make install
-    echo "======> compile svt finish <======"
 }
 
 compile_dav1d()
 {
     #-- dav1d
-    cd $FFMPEGROOT
-    echo "======> compile dav1d <======"
-    if [ ! -d "dav1d/build" ]; then
-        mkdir dav1d/build
-    fi
-    cd dav1d/build
-    meson setup -Denable_tools=false -Denable_tests=false --default-library=static .. --prefix $FFMPEGPREFIX --libdir=$FFMPEGPREFIX/bin
+    wk_dir="dav1d/build"
+    create_dir ${wk_dir} && cd ${wk_dir}
+
+    meson setup \
+        -Denable_tools=false \
+        -Denable_tests=false \
+        --default-library=static \
+        .. \
+        --prefix $FFMPEGPREFIX \
+        --libdir=$FFMPEGPREFIX/lib
     ninja && ninja install
-    echo "======> compile dav1d finish <======"
 }
 
 compile_vmaf()
 {
     #-- vmaf
-    cd $FFMPEGROOT
-    echo "======> compile vmaf <======"
-    if [ ! -d "vmaf-2.1.1/libvmaf/build" ]; then
-        mkdir vmaf-2.1.1/libvmaf/build
-    fi
-    cd vmaf-2.1.1/libvmaf/build
-    meson setup -Denable_tests=false -Denable_docs=false --buildtype=release \
-        --default-library=static .. --prefix $FFMPEGPREFIX --bindir=$FFMPEGPREFIX/bin --libdir=$FFMPEGPREFIX/lib
+    wk_dir="vmaf-2.1.1/libvmaf/build"
+    create_dir ${wk_dir} && cd ${wk_dir}
+
+    meson setup -Denable_tests=false \
+        -Denable_docs=false \
+        --buildtype=release \
+        --default-library=static \
+        .. \
+        --prefix $FFMPEGPREFIX \
+        --bindir=$FFMPEGPREFIX/bin \
+        --libdir=$FFMPEGPREFIX/lib
     ninja && ninja install
-    echo "======> compile vmaf finish <======"
 }
 
 compile_ffmpeg()
 {
     #-- ffmpeg
-    cd $FFMPEGROOT
-    echo "======> compile ffmpeg <======"
-    if [ ! -d "ffmpeg/build" ]; then
-        mkdir ffmpeg/build
-    fi
-    cd ffmpeg/build
-    PATH="$FFMPEGPATH" PKG_CONFIG_PATH="FFMPEGPKGCONFIG" ../configure \
+    wk_dir="ffmpeg/build"
+    create_dir ${wk_dir} && cd ${wk_dir}
+
+    PATH="$FFMPEGPATH" PKG_CONFIG_PATH="${FFMPEGPKGCONFIG}" ../configure \
         --prefix=$FFMPEGPREFIX \
         --pkg-config-flags="--static" \
         --extra-cflags="-I $FFMPEGPREFIX/include" \
@@ -206,41 +195,48 @@ compile_ffmpeg()
         --enable-nonfree
     
     PATH="$FFMPEGPATH" make -j20 && make install
-    echo "======> compile ffmpeg finish <======"
+}
+
+compile_distributor()
+{
+    components=$1
+    cd $FFMPEGROOT
+
+    echo ""
+    if [ "${runOpt}" != "c" ]; then
+        read -p "continue? [y/n/c] def[y]:" runOpt
+        if [ "$runOpt" = "n" ];then exit 0; fi
+    fi
+
+    echo "======> compile ${components} <======"
+    build_func="compile_${components}"
+    echo "Build components: ${components}"
+    echo "Build components func: ${build_func}"
+    ${build_func}
+    res=$?
+    echo "======> compile ${components} finish res:${res} <======"
 }
 
 
 # ============> compile select <============
+runOpt=""
 setup_env
 case $1 in
-    'all')
-        compile_nasm
-        compile_x264
-        compile_x265
-        compile_vpx
-        compile_fdkaac
-        compile_opus
-        compile_aom
-        compile_svt
-        compile_dav1d
-        compile_vmaf
-        compile_ffmpeg
-        ;;
     'ffmpeg')
         compile_ffmpeg
         ;;
     *)
-        compile_nasm
-        compile_x264
-        compile_x265
-        compile_vpx
-        compile_fdkaac
-        compile_opus
-        compile_aom
-        compile_svt
-        compile_dav1d
-        compile_vmaf
-        compile_ffmpeg
+        compile_distributor "nasm"
+        compile_distributor "x264"
+        compile_distributor "x265"
+        compile_distributor "vpx"
+        compile_distributor "fdkaac"
+        compile_distributor "opus"
+        compile_distributor "aom"
+        compile_distributor "svt"
+        compile_distributor "dav1d"
+        compile_distributor "vmaf"
+        compile_distributor "ffmpeg"
 esac
 
 set +e
