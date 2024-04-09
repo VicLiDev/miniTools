4de241e8ead7ce0b8fb81ca159a463c3e128da2e
Hongjin Li <vic.hong@rock-chips.com>
2024-04-09 20:20:36 +0800

    update tar mpp tool

--- 1.compileRun/2.tarMpp.sh
+++ 1.compileRun/2.tarMpp.sh
@@ -13,6 +13,7 @@
 mppRoot="${HOME}/Projects/mpp"
 tarPkgName="mpplib.tar.gz"
 mppLibDir="mpplib"
+pushTool="push.sh"
 
 
 android_32_dir="${mppLibDir}/android_32"
@@ -78,6 +79,17 @@ function update_android()
     update_file ${mppRoot}/build/android/aarch64/test/mpi_dec_multi_test  ${android_64_dir}
     update_file ${mppRoot}/build/android/aarch64/test/mpi_enc_test        ${android_64_dir}
     update_file ${mppRoot}/build/android/aarch64/test/mpi_enc_mt_test     ${android_64_dir}
+
+    echo "adb push android_32/libmpp.so          /vendor/lib32" >> ${mppLibDir}/${pushTool}
+    echo "adb push android_32/libvpu.so          /vendor/lib32" >> ${mppLibDir}/${pushTool}
+    echo "adb push android_32/mpi_dec_test       /vendor/bin"   >> ${mppLibDir}/${pushTool}
+    echo "adb push android_32/mpi_enc_test       /vendor/bin"   >> ${mppLibDir}/${pushTool}
+    echo "adb push android_32/mpi_dec_mt_test    /vendor/bin"   >> ${mppLibDir}/${pushTool}
+    echo "adb push android_32/mpi_dec_multi_test /vendor/bin"   >> ${mppLibDir}/${pushTool}
+    echo "adb push android_32/mpi_enc_mt_test    /vendor/bin"   >> ${mppLibDir}/${pushTool}
+
+    echo "adb push android_64/libmpp.so          /vendor/lib64" >> ${mppLibDir}/${pushTool}
+    echo "adb push android_64/libvpu.so          /vendor/lib64" >> ${mppLibDir}/${pushTool}
 }
 
 function update_linux()
@@ -100,6 +112,26 @@ function update_linux()
     update_file ${mppRoot}/build/linux/aarch64/test/mpi_dec_multi_test         ${linux_64_dir}
     update_file ${mppRoot}/build/linux/aarch64/test/mpi_enc_test               ${linux_64_dir}
     update_file ${mppRoot}/build/linux/aarch64/test/mpi_enc_mt_test            ${linux_64_dir}
+
+    echo "adb push linux_32/librockchip_mpp.so.0   /usr/lib"     >> ${mppLibDir}/${pushTool}
+    echo "adb push linux_32/librockchip_vpu.so.0   /usr/lib"     >> ${mppLibDir}/${pushTool}
+    echo "adb push linux_32/mpi_dec_test           /usr/bin"     >> ${mppLibDir}/${pushTool}
+    echo "adb push linux_32/mpi_enc_test           /usr/bin"     >> ${mppLibDir}/${pushTool}
+    echo "adb push linux_32/mpi_dec_mt_test        /usr/bin"     >> ${mppLibDir}/${pushTool}
+    echo "adb push linux_32/mpi_dec_multi_test     /usr/bin"     >> ${mppLibDir}/${pushTool}
+    echo "adb push linux_32/mpi_enc_mt_test        /usr/bin"     >> ${mppLibDir}/${pushTool}
+    echo "adb push linux_32/librockchip_mpp.so.0   /oem/usr/lib" >> ${mppLibDir}/${pushTool}
+    echo "adb push linux_32/librockchip_vpu.so.0   /oem/usr/lib" >> ${mppLibDir}/${pushTool}
+    echo "adb push linux_32/mpi_dec_test           /oem/usr/bin" >> ${mppLibDir}/${pushTool}
+    echo "adb push linux_32/mpi_enc_test           /oem/usr/bin" >> ${mppLibDir}/${pushTool}
+    echo "adb push linux_32/mpi_dec_mt_test        /oem/usr/bin" >> ${mppLibDir}/${pushTool}
+    echo "adb push linux_32/mpi_dec_multi_test     /oem/usr/bin" >> ${mppLibDir}/${pushTool}
+    echo "adb push linux_32/mpi_enc_mt_test        /oem/usr/bin" >> ${mppLibDir}/${pushTool}
+
+    echo "adb push linux_64/librockchip_mpp.so.0   /usr/lib64"     >> ${mppLibDir}/${pushTool}
+    echo "adb push linux_64/librockchip_vpu.so.0   /usr/lib64"     >> ${mppLibDir}/${pushTool}
+    echo "adb push linux_64/librockchip_mpp.so.0   /oem/usr/lib64" >> ${mppLibDir}/${pushTool}
+    echo "adb push linux_64/librockchip_vpu.so.0   /oem/usr/lib64" >> ${mppLibDir}/${pushTool}
 }
 
 
