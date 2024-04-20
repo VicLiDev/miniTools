012d88b37740e0f8d0eed31f03b8de33fa6be3e2
Hongjin Li <872648180@qq.com>
2024-04-20 18:29:55 +0800

    add vpn

---
+++ 1.compileRun/2.conVpn.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#########################################################################
+# File Name: 2.conVpn.sh
+# Author: LiHongjin
+# mail: 872648180@qq.com
+# Created Time: Sat 20 Apr 2024 06:21:31 PM CST
+#########################################################################
+
+1. install vpnc is necessary
+maybe:
+sudo apt-get install network-manager-vpnc
+or
+sudo apt-get install vpnc
+
+2. usage
+input passwd manual:
+sudo vpnc-connect --gateway=<serverIp> --id <groupName> --username <userNmae>
+input passwd with clear text
+sudo vpnc-connect --gateway=<serverIp> --id <groupName> --secret "<pw>" --username <userNmae> --password "<pw>"
