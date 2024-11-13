7627179ae671e910b3d6e4382d3ab5c85b17612c
Hongjin Li <vic.hong@rock-chips.com>
2024-11-13 11:52:56 +0800

    updata cpu usage capCpuData tool

--- 1.compileRun/11.sysParaMon/capCpuData.sh
+++ 1.compileRun/11.sysParaMon/capCpuData.sh
@@ -21,6 +21,11 @@ echo "gen log file name: ${gen_logfname}"
 echo "ins log file name: ${ins_logfname}"
 echo "thd log file name: ${thd_logfname}"
 
+# 获取当前时间戳（毫秒级）
+current_time()
+{
+    date +%s%3N
+}
 
 loop=0
 
@@ -28,6 +33,9 @@ while true
 do
     loop=`expr ${loop} + 1`
     echo "Capture ins CPU data loop idx: ${loop}"
+
+    # 记录开始时间
+    start_time=$(current_time)
 
     # gne
     echo "Capture gen CPU data loop idx: ${loop}" >> ${gen_logfname}
@@ -46,7 +54,20 @@ do
     do
         echo "stat `cat ${process_thd_dir}/${thd_id}/stat`" >> ${thd_logfname}
     done
-    sleep 1
+
+    # 记录结束时间
+    end_time=$(current_time)
+    # 计算命令执行时间
+    elapsed_time=$((end_time - start_time))
+    # 计算下一次执行前的休眠时间
+    sleep_time=$((1000 - elapsed_time))
+    # 如果命令执行时间超过 1 秒，直接继续下一次执行
+    if [ ${sleep_time} -gt 0 ]; then
+        # BEGIN 是 awk 中的一个特殊块，它在处理任何输入数据之前执行一次。
+        # 在这个命令中，它用于在没有输入文件的情况下执行一个简单的计算。
+        # echo "sleep $(awk "BEGIN {print ${sleep_time}/1000}")s"
+        sleep $(awk "BEGIN {print ${sleep_time}/1000}")
+    fi
 done
 
 
