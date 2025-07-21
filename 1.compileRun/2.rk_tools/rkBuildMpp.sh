71a9d168eadef9d1a1f6f928c0119386f744da31
Hongjin Li <vic.hong@rock-chips.com>
2025-07-21 14:07:23 +0800

    rk_toos: support kmpp build

--- 1.compileRun/2.rk_tools/rkBuildMpp.sh
+++ 1.compileRun/2.rk_tools/rkBuildMpp.sh
@@ -16,17 +16,24 @@ sel_tag_mpp="rk_mpp_b: "
 sel_tag_mpp_ko="rk_mpp_ko_b: "
 
 plt_lst=(
-    "android_32"
-    "android_64"
-    "linux_32"
-    "linux_64"
-    "linux_x86_64"
-    "ko_develop2"
-    "ko_kmpp_develop"
+    "lib_android_32"
+    "lib_android_64"
+    "lib_linux_32"
+    "lib_linux_64"
+    "lib_linux_x86_64"
+    "ko_kmpp_develop_mkf_arm32_one_ko"
+    "ko_kmpp_develop_mkf_arm32_multi_ko"
+    "ko_kmpp_develop_mkf_arm64_one_ko"
+    "ko_kmpp_develop_mkf_arm64_multi_ko"
+    "ko_kmpp_develop_cmk_arm32_one_ko"
+    "ko_kmpp_develop_cmk_arm32_multi_ko"
+    "ko_kmpp_develop_cmk_arm64_one_ko"
+    "ko_kmpp_develop_cmk_arm64_multi_ko"
     "ko_kmpp"
+    "ko_develop2"
     )
 
-m_sel_arch=""
+m_sel=""
 
 kdir_lst=(
     "${HOME}/Projects/kernel"
@@ -38,7 +45,7 @@ m_sel_kdir=""
 
 linux_toolchain_dir="${HOME}/Projects/prebuilts"
 
-push_bins_to_device()
+function push_bins_to_device()
 {
     adbCmd="$1"
     file_dir="$2"
@@ -65,9 +72,9 @@ push_bins_to_device()
     fi
 }
 
-build_android_32()
+function build_lib_android_32()
 {
-    echo "======> selected ${m_sel_arch} <======"
+    echo "======> selected ${m_sel} <======"
     cd `git rev-parse --show-toplevel` \
         && cd build/android/arm \
         && ./make-Android.bash
@@ -97,9 +104,9 @@ build_android_32()
     fi
 }
 
-build_android_64()
+function build_lib_android_64()
 {
-    echo "======> selected ${m_sel_arch} <======"
+    echo "======> selected ${m_sel} <======"
     cd `git rev-parse --show-toplevel` \
         && cd build/android/aarch64 \
         && ./make-Android.bash
@@ -129,9 +136,9 @@ build_android_64()
     fi
 }
 
-build_linux_32()
+function build_lib_linux_32()
 {
-    echo "======> selected ${m_sel_arch} <======"
+    echo "======> selected ${m_sel} <======"
     linux_32_tc_dir="${linux_toolchain_dir}/toolchains/arm/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf"
     linux_32_tc="${linux_32_tc_dir}/bin/arm-linux-gnueabihf-"
     echo "toolchain: ${linux_32_tc}"
@@ -171,9 +178,9 @@ build_linux_32()
     fi
 }
 
-build_linux_64()
+function build_lib_linux_64()
 {
-    echo "======> selected ${m_sel_arch} <======"
+    echo "======> selected ${m_sel} <======"
     linux_64_tc_dir="${linux_toolchain_dir}/toolchains/aarch64/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu"
     linux_64_tc="${linux_64_tc_dir}/bin/aarch64-none-linux-gnu-"
     echo "toolchain: ${linux_64_tc}"
@@ -212,9 +219,9 @@ build_linux_64()
     fi
 }
 
-build_linux_x86_64()
+function build_lib_linux_x86_64()
 {
-    echo "======> selected ${m_sel_arch} <======"
+    echo "======> selected ${m_sel} <======"
     cd `git rev-parse --show-toplevel` \
         && cd build/linux/x86_64 \
         && ./make-Makefiles.bash
@@ -227,7 +234,7 @@ build_linux_x86_64()
     fi
 }
 
-rmmod_ko()
+function rmmod_ko()
 {
     ko_mod_name="$1"
     if [ -n "`${adbCmd} shell lsmod | grep ${ko_mod_name}`" ]; then
@@ -236,7 +243,7 @@ rmmod_ko()
     fi
 }
 
-insmod_ko()
+function insmod_ko()
 {
     ko_mod_name="$1"
     ko_mod_dir="$2"
@@ -244,107 +251,245 @@ insmod_ko()
     ${adbCmd} shell insmod ${ko_mod_dir}/${ko_mod_name}.ko
 }
 
-build_ko_develop2()
+function push_ko_to_device()
 {
-    echo "======> selected ${m_sel_arch} <======"
-    select_node "${sel_tag_mpp_ko}" "kdir_lst" "m_sel_kdir" "kernel dir"
+    build_sys="$1"
+    build_type="$2"
+    arch="$3"
 
-    info_list=`echo -e "\n\n" | bash ~/bin/rkBuildKer.sh --dir ${m_sel_kdir} --env \
-        | grep -E "toolchains|m_make"`
-    toolchains="`echo -e ${info_list} | awk '{print $2}'`"
-    make_cmd=${info_list#*m_make: }
-    echo "toolchains: ${toolchains}"
-    echo "make_cmd: ${make_cmd}"
+    adbCmd=""
+    [ -z "${cmd_ins_dev}" ] && { adbCmd=$(adbs); } || { adbCmd=$(adbs --idx ${cmd_ins_dev}); }
+    [ -z "${adbCmd}" ] && exit 0
 
-    # build
-    cd `git rev-parse --show-toplevel` \
-        && cd build/kmpp/aarch64 \
-        && ./make-Kbuild.sh --kernel ${m_sel_kdir} \
-            --toolchain ${toolchains} --ndk ${toolchains}
-
-    if [ $? -eq 0 ]; then
-        echo "======> build mpp sucess! <======"
-    else
-        echo "======> build mpp error! <======"
-        return 1
+    f_result=$(${adbCmd} shell 'if [ -f /oem/usr/ko/insmod_ko.sh ]; then echo exists; else echo missing; fi')
+    if [[ "${f_result}" == *exists* ]]; then
+        echo "mv /oem/usr/ko/insmod_ko.sh /oem/usr/ko/insmod_ko.sh.bak and reboot maybe necessary"
+        return
     fi
+
+    if [ "${build_sys}" = "mkf" ]; then
+        # test
+        ${adbCmd} push ./kmpp_test.ko           /data
+        ${adbCmd} push ./kmpi_vsp_test.ko       /data
+        ${adbCmd} push ./kmpi_enc_chan_test.ko  /data
+        ${adbCmd} push ./kmpi_enc_test.ko       /data
+        ${adbCmd} push ./osal_test.ko           /data
+        ${adbCmd} push ./kmpp_enc_test.ko       /data
+        ${adbCmd} push ./stub_test.ko           /data
+        # ko
+        if [ "${build_type}" = "multi" ]; then
+            ${adbCmd} push ./kmpp_osal.ko /data
+            ${adbCmd} push ./kmpp_sys.ko  /data
+            ${adbCmd} push ./rk_vcodec.ko /data
+        fi
+        ${adbCmd} push ./kmpp.ko /data
+    elif [ "${build_sys}" = "cmk" ]; then
+        # test
+        ${adbCmd} push ./test/kmpp_test/build/kmpp_test.ko                   /data
+        ${adbCmd} push ./test/kmpi_vsp_test/build/kmpi_vsp_test.ko           /data
+        ${adbCmd} push ./test/kmpi_enc_chan_test/build/kmpi_enc_chan_test.ko /data
+        ${adbCmd} push ./test/kmpi_enc_test/build/kmpi_enc_test.ko           /data
+        ${adbCmd} push ./test/osal_test/build/osal_test.ko                   /data
+        ${adbCmd} push ./test/kmpp_enc_test/build/kmpp_enc_test.ko           /data
+        ${adbCmd} push ./test/sym_test/sym_test/build/sym_test.ko            /data
+        ${adbCmd} push ./test/sym_test/sym1/build/sym1.ko                    /data
+        ${adbCmd} push ./test/sym_test/sym2/build/sym2.ko                    /data
+        ${adbCmd} push ./test/sym_test/sym3/build/sym3.ko                    /data
+        ${adbCmd} push ./test/sym_test/sym4/build/sym4.ko                    /data
+        # ko
+        if [ "${build_type}" = "multi" ]; then
+            ${adbCmd} push ./osal/build/osal.ko             /data
+            ${adbCmd} push ./sys/build/sys.ko               /data
+            ${adbCmd} push ./mpp_service/build/rk_vcodec.ko /data
+        fi
+        ${adbCmd} push ./build/kmpp.ko /data
+    else
+        echo "build sys have not set"
+        return
+    fi
+
+    # rmmod
+    # test
+    rmmod_ko stub_test
+    rmmod_ko sym_test
+    rmmod_ko sym1
+    rmmod_ko sym2
+    rmmod_ko sym3
+    rmmod_ko sym4
+    rmmod_ko kmpp_test
+    rmmod_ko kmpi_vsp_test
+    rmmod_ko kmpi_enc_chan_test
+    rmmod_ko kmpi_enc_test
+    rmmod_ko osal_test
+    rmmod_ko kmpp_enc_test
+    # mod
+    rmmod_ko kmpp
+    rmmod_ko rk_vcodec
+    rmmod_ko kmpp_sys
+    rmmod_ko kmpp_osal
+    rmmod_ko sys
+    rmmod_ko osal
+
+    # insmod
+    # mod
+    if [ "${build_type}" = "multi" ]; then
+        if [ "${build_sys}" = "mkf" ]; then
+            insmod_ko kmpp_osal /data
+            insmod_ko kmpp_sys  /data
+            insmod_ko rk_vcodec /data
+        elif [ "${build_sys}" = "cmk" ]; then
+            insmod_ko osal      /data
+            insmod_ko sys       /data
+            insmod_ko rk_vcodec /data
+        fi
+    fi
+    insmod_ko kmpp /data
+    # test
+    # insmod_ko kmpp_test          /data
+    # insmod_ko kmpi_vsp_test      /data
+    # insmod_ko kmpi_enc_chan_test /data
+    # insmod_ko kmpi_enc_test      /data
+    # insmod_ko osal_test          /data
+    # insmod_ko kmpp_enc_test      /data
+    # if [ "${build_sys}" = "mkf" ]; then
+    #     insmod_ko stub_test  /data
+    # elif [ "${build_sys}" = "cmk" ]; then
+    #     insmod_ko sym_test /data
+    #     insmod_ko sym1     /data
+    #     insmod_ko sym2     /data
+    #     insmod_ko sym3     /data
+    #     insmod_ko sym4     /data
+    # fi
 }
 
-build_ko_kmpp_develop()
+function build_ko_kmpp_develop_mkf()
 {
-    echo "======> selected ${m_sel_arch} <======"
+    echo "======> selected ${m_sel} <======"
     select_node "${sel_tag_mpp_ko}" "kdir_lst" "m_sel_kdir" "kernel dir"
 
     info_list=`echo -e "\n\n" | bash ~/bin/rkBuildKer.sh --dir ${m_sel_kdir} --env \
         | grep -E "toolchains|m_make"`
     toolchains="`echo -e ${info_list} | awk '{print $2}'`"
     make_cmd=${info_list#*m_make: }
-    echo "toolchains: ${toolchains}"
-    echo "make_cmd: ${make_cmd}"
+    cross_cmp="${toolchains}/$(ls -1 ${toolchains} | grep gcc$ | sed 's/gcc//g')"
+    m_arch="$1"
+    mod_build_type="$2"
+    enable_one_ko="y"
+
+    [ ${mod_build_type} = "one" ] && enable_one_ko="y"
+    [ ${mod_build_type} = "multi" ] && enable_one_ko="n"
+
+    echo "toolchains:  ${toolchains}"
+    echo "make_cmd:    ${make_cmd}"
+    echo "kernel dir:  ${m_sel_kdir}"
+    echo "cross cmp:   ${cross_cmp}"
+    echo "arch:        ${m_arch}"
+    echo "module type: ${mod_build_type}"
 
     # build
-    cd `git rev-parse --show-toplevel` \
-        && cd build/aarch64 \
-        && ./make-Kbuild.sh --kernel ${m_sel_kdir} \
-            --toolchain ${toolchains} --ndk ${toolchains}
+    cd `git rev-parse --show-toplevel`
+    export BUILD_ONE_KO=${enable_one_ko}
+    export KERNEL_DIR=${m_sel_kdir}
+    export CROSS_COMPILE=${cross_cmp}
+    export ARCH=${m_arch}
+    make all
 
     if [ $? -eq 0 ]; then
         [ "${cmd_install}" == "false" ] && return
 
         echo "======> push lib and demo to dev <======"
         # install
-        adbCmd=""
-        [ -z "${cmd_ins_dev}" ] && { adbCmd=$(adbs); } || { adbCmd=$(adbs --idx ${cmd_ins_dev}); }
-        [ -z "${adbCmd}" ] && exit 0
-
-        ${adbCmd} push sys/build/sys.ko                            /data
-        ${adbCmd} push osal/build/osal.ko                          /data
-        ${adbCmd} push mpp_service/build/kmpp.ko                   /data
-        ${adbCmd} push test/sym_test/sym4/build/sym4.ko            /data
-        ${adbCmd} push test/sym_test/sym1/build/sym1.ko            /data
-        ${adbCmd} push test/sym_test/sym3/build/sym3.ko            /data
-        ${adbCmd} push test/sym_test/sym2/build/sym2.ko            /data
-        ${adbCmd} push test/osal_test/build/osal_test.ko           /data
-        ${adbCmd} push test/kmpi_enc_test/build/kmpi_enc_test.ko   /data
-        ${adbCmd} push test/kmpp_enc_test/build/kmpp_enc_test.ko   /data
-        ${adbCmd} push test/kmpp_test/build/kmpp_test.ko           /data
-        ${adbCmd} push test/sym_test/sym_test/build/sym_test.ko    /data
-
-        # rmmod_ko kmpi_enc_test
-        # rmmod_ko kmpp_enc_test
-        # rmmod_ko kmpp_test
-        # rmmod_ko sym_test
-        # rmmod_ko osal_test
-        rmmod_ko sym1
-        rmmod_ko sym3
-        rmmod_ko sym2
-        rmmod_ko sym4
-        rmmod_ko kmpp
-        rmmod_ko sys
-        rmmod_ko osal
-
-        insmod_ko osal           /data
-        insmod_ko sys            /data
-        insmod_ko kmpp           /data
-        insmod_ko sym4           /data
-        insmod_ko sym3           /data
-        insmod_ko sym2           /data
-        insmod_ko sym1           /data
-        # insmod_ko osal_test      /data
-        # insmod_ko sym_test       /data
-        # insmod_ko kmpp_test      /data
-        # insmod_ko kmpp_enc_test  /data
-        # insmod_ko kmpi_enc_test  /data
+        push_ko_to_device "mkf" "${mod_build_type}" "${m_arch}"
     else
         echo "======> build mpp error! <======"
         return 1
     fi
 }
 
-# 5.10 1106_linux
-build_ko_kmpp()
+function build_ko_kmpp_develop_mkf_arm32_one_ko()
 {
-    echo "======> selected ${m_sel_arch} <======"
+    build_ko_kmpp_develop_mkf "arm" "one"
+}
+
+function build_ko_kmpp_develop_mkf_arm32_multi_ko()
+{
+    build_ko_kmpp_develop_mkf "arm" "multi"
+}
+
+function build_ko_kmpp_develop_mkf_arm64_one_ko()
+{
+    build_ko_kmpp_develop_mkf "arm64" "one"
+}
+
+function build_ko_kmpp_develop_mkf_arm64_multi_ko()
+{
+    build_ko_kmpp_develop_mkf "arm64" "multi"
+}
+
+function build_ko_kmpp_develop_cmk()
+{
+    echo "======> selected ${m_sel} <======"
+    select_node "${sel_tag_mpp_ko}" "kdir_lst" "m_sel_kdir" "kernel dir"
+
+    info_list=`echo -e "\n\n" | bash ~/bin/rkBuildKer.sh --dir ${m_sel_kdir} --env \
+        | grep -E "toolchains|m_make"`
+    toolchains="`echo -e ${info_list} | awk '{print $2}'`"
+    make_cmd=${info_list#*m_make: }
+    m_arch="$1"
+    mod_build_type="$2"
+    build_wk_dir=""
+
+    echo "toolchains:  ${toolchains}"
+    echo "make_cmd:    ${make_cmd}"
+    echo "kernel dir:  ${m_sel_kdir}"
+    echo "arch:        ${m_arch}"
+    echo "module type: ${mod_build_type}"
+
+    # build
+    [ ${m_arch} = "arm" ]   && build_wk_dir="build/arm"
+    [ ${m_arch} = "arm64" ] && build_wk_dir="build/aarch64"
+    export MODULE_TYPE=${mod_build_type}
+    cd `git rev-parse --show-toplevel` \
+        && cd ${build_wk_dir} \
+        && ./make-Kbuild.sh --kernel ${m_sel_kdir} \
+            --toolchain ${toolchains}
+
+    if [ $? -eq 0 ]; then
+        [ "${cmd_install}" == "false" ] && return
+
+        echo "======> push lib and demo to dev <======"
+        # install
+        push_ko_to_device "cmk" "${mod_build_type}" "${m_arch}"
+    else
+        echo "======> build mpp error! <======"
+        return 1
+    fi
+}
+
+function build_ko_kmpp_develop_cmk_arm32_one_ko()
+{
+    build_ko_kmpp_develop_cmk "arm" "one"
+}
+
+function build_ko_kmpp_develop_cmk_arm32_multi_ko()
+{
+    build_ko_kmpp_develop_cmk "arm" "multi"
+}
+
+function build_ko_kmpp_develop_cmk_arm64_one_ko()
+{
+    build_ko_kmpp_develop_cmk "arm64" "one"
+}
+
+function build_ko_kmpp_develop_cmk_arm64_multi_ko()
+{
+    build_ko_kmpp_develop_cmk "arm64" "multi"
+}
+
+# 5.10 1106_linux
+function build_ko_kmpp()
+{
+    echo "======> selected ${m_sel} <======"
     select_node "${sel_tag_mpp_ko}" "kdir_lst" "m_sel_kdir" "kernel dir"
 
     info_list=`echo -e "\n\n" | bash ~/bin/rkBuildKer.sh --dir ${m_sel_kdir} --env \
@@ -360,6 +505,32 @@ build_ko_kmpp()
     export PATH=${toolchains}:${PATH}
     cd `git rev-parse --show-toplevel` \
         && ${kmpp_build_cmd}
+
+    if [ $? -eq 0 ]; then
+        echo "======> build mpp sucess! <======"
+    else
+        echo "======> build mpp error! <======"
+        return 1
+    fi
+}
+
+function build_ko_develop2()
+{
+    echo "======> selected ${m_sel} <======"
+    select_node "${sel_tag_mpp_ko}" "kdir_lst" "m_sel_kdir" "kernel dir"
+
+    info_list=`echo -e "\n\n" | bash ~/bin/rkBuildKer.sh --dir ${m_sel_kdir} --env \
+        | grep -E "toolchains|m_make"`
+    toolchains="`echo -e ${info_list} | awk '{print $2}'`"
+    make_cmd=${info_list#*m_make: }
+    echo "toolchains: ${toolchains}"
+    echo "make_cmd: ${make_cmd}"
+
+    # build
+    cd `git rev-parse --show-toplevel` \
+        && cd build/kmpp/aarch64 \
+        && ./make-Kbuild.sh --kernel ${m_sel_kdir} \
+            --toolchain ${toolchains} --ndk ${toolchains}
 
     if [ $? -eq 0 ]; then
         echo "======> build mpp sucess! <======"
@@ -431,11 +602,11 @@ source ${prjRootDir}/0.general_tools/0.select_node.sh
 proc_paras $@
 
 if [ -z "${cmd_sel_plt}" ]; then
-    select_node "${sel_tag_mpp}" "plt_lst" "m_sel_arch" "platform"
+    select_node "${sel_tag_mpp}" "plt_lst" "m_sel" "platform"
 else
-    m_sel_arch="${plt_lst[${cmd_sel_plt}]}"
+    m_sel="${plt_lst[${cmd_sel_plt}]}"
 fi
-build_${m_sel_arch}
+build_${m_sel}
 
 
 # set +e
