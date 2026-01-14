4b44ab64e97acfabc5ab3f8646e91caf535aaefb
VicLiDev <872648180@qq.com>
2026-01-14 17:39:25 +0800

    data_proc: update buffer ana tool

--- 2.dataProc/17.rk_dbg_tools/buffer_grp_ana/analyze.sh
+++ 2.dataProc/17.rk_dbg_tools/buffer_grp_ana/analyze.sh
@@ -6,26 +6,75 @@
 # Created Time: Fri  8 Nov 10:39:34 2024
 #########################################################################
 
-logfile="${HOME}/Downloads/log.txt"
+# 放开打印：
+# setprop mpp_sys_cfg_debug 0x10 && setprop vendor.mpp_sys_cfg_debug 0x10
+# setprop mpp_sys_cfg_debug 0 && setprop vendor.mpp_sys_cfg_debug 0
+# export mpp_sys_cfg_debug=0x10
+# export mpp_sys_cfg_debug=0
 
-grp=(`cat ${logfile} | grep mpp_buffer | sed 's/.*mpp_buffer: group//g' | sed 's/\r//g' | awk '{print $1}' | sort -n | uniq`)
-
-echo ${grp[@]}
+[ -z "$1" ] && { echo "usage: <exe> <org_log_file>"; exit 1; }
+[ ! -f "$1" ] && { echo "usage: <exe> <org_log_file>"; exit 1; }
+logfile="${1}"
 
 runOpt=""
+
+grp=(`grep mpp_buffer ${logfile} \
+    | sed 's/.*mpp_buffer: *//g' \
+    | sed 's/group *//g' \
+    | awk '{print $1}' \
+    | sort -n | uniq`)
+
+echo "======> group_list: ${grp[@]} <======"
 for cur_grp in ${grp[@]}
 do
-    grp_idxs=(`cat ${logfile} | grep "group *${cur_grp}" | sed "s/.*group *${cur_grp} buffer//g" | awk '{print $1}' | sort -n | uniq`)
-    for cur_idx in ${grp_idxs[@]}
-    do
-        cur_file="grp${cur_grp}_idx${cur_idx}.txt"
-        echo ${cur_file}
-        cat ${logfile} | grep "group *${cur_grp}" | grep "group *${cur_grp} buffer *${cur_idx}"
+    buf_idxs=(`grep mpp_buffer ${logfile} \
+        | sed 's/.*mpp_buffer: *//g' \
+        | grep "group * ${cur_grp} buffer" \
+        | sed "s/.*group *${cur_grp} buffer *//g" \
+        | awk '{print $1}' \
+        | grep -E '^[0-9]+' \
+        | sort -n | uniq`)
 
+    if [ -z "${buf_idxs}" ]
+    then
+        echo "------> grp:${cur_grp} buf_idx is null <------"
+        grep mpp_buffer ${logfile} \
+            | sed 's/.*mpp_buffer: *//g' \
+            | grep "group * ${cur_grp} "
+        echo "------> grp:${cur_grp} buf_idx is null <------"
 
         if [ "${runOpt}" != "c" ]; then
             read -p "continue? [y/n/c] def[y]:" runOpt
             if [ "$runOpt" = "n" ];then exit 0; fi
         fi
+
+        continue
+    fi
+
+    echo "======> group ${cur_grp} buf_idx_list: ${buf_idxs[@]} <======"
+    for cur_buf_idx in ${buf_idxs[@]}
+    do
+        buf_fds=(`grep mpp_buffer ${logfile} \
+            | sed 's/.*mpp_buffer: *//g' \
+            | grep "group * ${cur_grp} buffer * ${cur_buf_idx} fd" \
+            | sed "s/group * ${cur_grp} buffer * ${cur_buf_idx} fd *//g" \
+            | awk '{print $1}' \
+            | grep -E '^[0-9]' \
+            | sort -n | uniq`)
+
+        echo "======> group ${cur_grp} buf_idx ${cur_buf_idx} fd_list: ${buf_fds[@]} <======"
+        for cur_fd in ${buf_fds}
+        do
+            echo "------> grp:${cur_grp}  buf_idx:${buf_idxs}  fd:${cur_fd} <------"
+            grep mpp_buffer ${logfile} \
+                | sed 's/.*mpp_buffer: *//g' \
+                | grep "group * ${cur_grp} buffer * ${cur_buf_idx} fd * ${cur_fd}"
+            echo "------> grp:${cur_grp}  buf_idx:${buf_idxs}  fd:${cur_fd} <------"
+
+            if [ "${runOpt}" != "c" ]; then
+                read -p "continue? [y/n/c] def[y]:" runOpt
+                if [ "$runOpt" = "n" ];then exit 0; fi
+            fi
+        done
     done
 done
