40960f7b96e2dd4175540ba69d0cb5d1dc248b11
VicLiDev <872648180@qq.com>
2026-01-23 14:11:07 +0800

    shell init cfg support conda

--- 1.compileRun/0.gen_cmd_cfg.sh
+++ 1.compileRun/0.gen_cmd_cfg.sh
@@ -80,9 +80,26 @@ else
 fi
 # opencode
 export PATH=${HOME}/.opencode/bin:${PATH}
-# ======> miniforge3
-if [ -f "${HOME}/miniforge3/etc/profile.d/mamba.sh" ]; then
-    . "${HOME}/miniforge3/etc/profile.d/mamba.sh"
+# ======> conda
+conda_root=""
+[ -e "${HOME}/miniforge3" ] && conda_root="${HOME}/miniforge3"
+[ -e "${HOME}/anaconda3" ] && conda_root="${HOME}/anaconda3"
+if [ -n "${conda_root}" ]; then
+    __conda_setup="$("${conda_root}/bin/conda" 'shell.zsh' 'hook' 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
+    else
+        if [ -f "${conda_root}/etc/profile.d/conda.sh" ]; then
+            . "${conda_root}/etc/profile.d/conda.sh"
+        else
+            export PATH="${conda_root}/bin:${PATH}"
+        fi
+    fi
+    unset __conda_setup
+
+    if [ -f "${conda_root}/etc/profile.d/mamba.sh" ]; then
+        . "${conda_root}/etc/profile.d/mamba.sh"
+    fi
 fi
 
 # =============================================================================
