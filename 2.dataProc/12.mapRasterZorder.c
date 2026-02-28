4a5a73b98748b82036ab88e1cc5516cc13685ffc
VicLiDev <872648180@qq.com>
2026-02-28 17:21:03 +0800

    dataProc: update mapRasterZorder tool

--- 2.dataProc/12.mapRasterZorder.c
+++ 2.dataProc/12.mapRasterZorder.c
@@ -9,7 +9,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-void file(int w, int h, int *buf)
+void init_raster_index(int w, int h, int *buf)
 {
     int x, y;
 
@@ -36,7 +36,7 @@ void dump(int w, int h, int *buf)
 //      1: raster loc to zorder loc
 //      2: zorder loc to raster loc
 // intput x, y and output x, y use same general idx
-void mapCalc(int w, int x, int y, int *mx, int *my, int calc_type)
+void map_calc(int w, int x, int y, int *mx, int *my, int calc_type)
 {
     int genIdx = 0;
     int rem = 0;
@@ -78,14 +78,14 @@ void mapCalc(int w, int x, int y, int *mx, int *my, int calc_type)
 }
 
 
-void mapToZorder(int w, int h, int *raster_buf, int *zorder_buf)
+void map2zorder(int w, int h, int *raster_buf, int *zorder_buf)
 {
     int x, y;
     int mx, my;
 
     for (y = 0; y < h; y++) {
         for (x = 0; x < w; x++) {
-            mapCalc(w, x, y, &mx, &my, 0);
+            map_calc(w, x, y, &mx, &my, 0);
             zorder_buf[mx + my * w] = raster_buf[x + y * w];
         }
     }
@@ -93,14 +93,14 @@ void mapToZorder(int w, int h, int *raster_buf, int *zorder_buf)
     return;
 }
 
-void mapToRaster(int w, int h, int *zorder_buf, int *raster_buf)
+void map2raster(int w, int h, int *zorder_buf, int *raster_buf)
 {
     int x, y;
     int mx, my;
 
     for (y = 0; y < h; y++) {
         for (x = 0; x < w; x++) {
-            mapCalc(w, x, y, &mx, &my, 1);
+            map_calc(w, x, y, &mx, &my, 1);
             raster_buf[mx + my * w] = zorder_buf[x + y * w];
         }
     }
@@ -108,7 +108,7 @@ void mapToRaster(int w, int h, int *zorder_buf, int *raster_buf)
     return;
 }
 
-void mapTest(int w, int h)
+void map_conv(int w, int h)
 {
     int unit_cnt = 128 * 128; // 0x4000  16384
     int *raster_buf = NULL;
@@ -119,17 +119,17 @@ void mapTest(int w, int h)
     memset(raster_buf, 0, sizeof(int) * unit_cnt);
     memset(zorder_buf, 0, sizeof(int) * unit_cnt);
 
-    file(w, h, raster_buf);
+    init_raster_index(w, h, raster_buf);
     printf("======> orign data (raster order) <======\n");
     dump(w, h, raster_buf);
     printf("\n");
 
-    mapToZorder(w, h, raster_buf, zorder_buf);
+    map2zorder(w, h, raster_buf, zorder_buf);
     printf("======> map to zorder <======\n");
     dump(w, h, zorder_buf);
     printf("\n");
 
-    mapToRaster(w, h, zorder_buf, raster_buf);
+    map2raster(w, h, zorder_buf, raster_buf);
     printf("======> map to raster order <======\n");
     dump(w, h, raster_buf);
     printf("\n");
@@ -138,12 +138,39 @@ void mapTest(int w, int h)
     free(zorder_buf);
 }
 
+static void print_usage(const char *prog)
+{
+    printf("Usage: %s <size>\n", prog);
+    printf("\n");
+    printf("Convert between raster order and z-order mapping.\n");
+    printf("\n");
+    printf("Arguments:\n");
+    printf("  size    Size of the square matrix (width == height, default: 8)\n");
+    printf("\n");
+    printf("Example:\n");
+    printf("  %s 8\n", prog);
+    printf("  %s 16\n", prog);
+    printf("  %s 128\n", prog);
+}
+
 int main(int argc, char *argv[])
 {
-    (void)argc;
-    (void)argv;
-    // mapTest(4, 4);
-    mapTest(16, 16);
-    // mapTest(128, 128);
+    int size = 8;
+
+    if (argc >= 2 && (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)) {
+        print_usage(argv[0]);
+        return 0;
+    }
+
+    if (argc >= 2) {
+        size = atoi(argv[1]);
+        if (size <= 0) {
+            fprintf(stderr, "Error: size must be a positive integer\n");
+            return 1;
+        }
+    }
+
+    map_conv(size, size);
+
     return 0;
 }
