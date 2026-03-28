2a4fb514d592e7cc30fdc92a584fffe2a08be9b7
VicLiDev <872648180@qq.com>
2026-03-28 15:45:22 +0800

    init: add ffmpeg build tools

--- init_tools.sh
+++ init_tools.sh
@@ -80,6 +80,22 @@ function init_general_tools()
 }
 
 # =============================================================================
+#  build/run tools
+# =============================================================================
+
+function init_build_run_tools()
+{
+    tools_dir="${repo_root}/1.compileRun"
+
+    create_link_to_bin ${tools_dir}/01.ffmpeg_build/ffmpeg_build_all.sh  m_ffmpeg_bd.sh
+    create_link_to_bin ${tools_dir}/01.ffmpeg_build/ffmpeg_build.sh      m_ffmpeg_bd_smp.sh
+
+    echo
+    echo "==> build/run tools: for prj"
+    echo 'ln -s ${HOME}/bin/m_ffmpeg_bd_smp.sh .prjBuild.sh'
+}
+
+# =============================================================================
 #  data proc tools
 # =============================================================================
 
@@ -194,6 +210,7 @@ function init_shell()
 get_repo_root
 init_source_tools
 init_general_tools
+init_build_run_tools
 init_data_proc_tools
 init_rk_tools
 init_shell
