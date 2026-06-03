3fd04430a2923ba6e2855f8084d23258442e4f67
VicLiDev <872648180@qq.com>
2026-06-03 10:44:04 +0800

    update stress_test name

--- init_tools.sh
+++ init_tools.sh
@@ -99,7 +99,7 @@ function init_build_run_tools()
     create_link_to_bin ${tools_dir}/01.ffmpeg_build/ffmpeg_build_all.sh  m_ffmpeg_bd.sh
     create_link_to_bin ${tools_dir}/01.ffmpeg_build/ffmpeg_build_smp.sh  m_ffmpeg_smp.sh
 
-    create_link_to_bin ${tools_dir}/6.stress_test.py                     m_stree_test.py
+    create_link_to_bin ${tools_dir}/06.stress_test.py                    m_stree_test.py
     create_link_to_bin ${tools_dir}/16.usbinfo.sh                        m_usbinfo.sh
 
     echo "build/run tools: for prj usage"
